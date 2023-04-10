fa6ea437d5280ef09753eb61dba097dfc253901f
˚ ༘♡ ⋆｡˚niki.141--｡˚ <129641020+niki15-1@users.noreply.github.com>
2023-04-10 21:44:52 +0800

    Add files via upload
    open with safari
    audio will not work on chrome :)))

---
+++ jumpscare/jumpscare.js
@@ -0,0 +1,14 @@
+var firstName = "Hannah Nicole Orlina";
+var lastname = "Sison";
+var school = "PHILIPPINE SCIENCE HIGH SCHOOL SOUTHERN MINDANAO CAMPUS";
+var cry= "DO NOT ENTER";
+
+
+alert("BEWARE");
+alert("My name is "+firstName+" "+lastname);
+alert("I go to school at "+ school);
+alert("I am pretending to make a website for schoolworks");
+alert("PLEASE "+cry);
+alert("BEWARE");
+alert('I REPEAT '+cry);
+
